15256a1c22969bffadb29ac7dcdd8a5f95b1a9a6
VH🔥 <vherman@vf-soft.com>
2022-10-26 10:24:05 +0300

    add crdgen.sh

---
+++ hack/crdgen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu -o pipefail
+
+source $(dirname $0)/library.sh
+ensure_vendor
+
+if [ "$(command -v controller-gen)" = "" ]; then
+  go install sigs.k8s.io/controller-tools/cmd/controller-gen
+fi
+
+header "Generating CRDs"
+$(go env GOPATH)/bin/controller-gen crd:crdVersions=v1,maxDescLen=262143 paths=./pkg/apis/... output:dir=config/base/crds/full
+
+cp config/base/crds/full/qinkube.prettysolution.github.com*.yaml config/base/crds/minimal/
+
+find config/base/crds/minimal -name 'qinkube.prettysolution.github.com*.yaml' | while read -r file; do
+  echo "Patching ${file}"
+  # remove junk fields
+  go run ./hack/crdgen cleancrd "$file"
+done
+
